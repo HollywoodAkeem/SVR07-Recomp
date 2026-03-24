d701cb82bbf54b231c7f72e428ab6e4f06cc2302
HollywoodAkeem <hollywoodakeem@gmail.com>
2026-03-24 00:35:09 -0400

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include "generated/svr07_config.h"
+#include "generated/svr07_init.h"
+#include "svr07_app.h"
+
+REX_DEFINE_APP(svr07, Svr07App::Create)
---
+++ src/svr07_app.h
@@ -0,0 +1,27 @@
+// svr07 - ReXGlue Recompiled Project
+//
+// This file is yours to edit. 'rexglue migrate' will NOT overwrite it.
+// Customize your app by overriding virtual hooks from rex::ReXApp.
+
+#pragma once
+
+#include <rex/rex_app.h>
+
+class Svr07App : public rex::ReXApp {
+ public:
+  using rex::ReXApp::ReXApp;
+
+  static std::unique_ptr<rex::ui::WindowedApp> Create(
+      rex::ui::WindowedAppContext& ctx) {
+    return std::unique_ptr<Svr07App>(
+        new Svr07App(ctx, "svr07", PPCImageConfig));
+  }
+
+  // Override virtual hooks for customization:
+  // void OnPreSetup(rex::RuntimeConfig& config) override {}
+  // void OnLoadXexImage(std::string& xex_image) override {}
+  // void OnPostSetup() override {}
+  // void OnCreateDialogs(rex::ui::ImGuiDrawer* drawer) override {}
+  // void OnShutdown() override {}
+  // void OnConfigurePaths(rex::PathConfig& paths) override {}
+};
